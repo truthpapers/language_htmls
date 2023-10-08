fbb0d6d396eff38db0fde2240014eff7261ffd4d
truthpapers <tableofdawah@outlook.com>
2023-10-08 21:51:08 +0200

    performance

---
+++ data.js
@@ -0,0 +1,194 @@
+export const oxford3000 = {
+    "a": {
+        "translation": "أداة للتنكير، يقابلها بالعربية التنوين",
+        "examples": ["a student", "a teacher", "There's a visitor", "a knife and fork"],
+        "examplesToAr": ["طالبٌ", "معلمٌ", "هناك زائرٌ", "سكينة وشوكة"],
+        "dict": {
+            "a": "أداة للتنكير، يقابلها بالعربية التنوين",
+            "student": "طالب",
+            "teacher": "معلم",
+            "There's": "يوجد أو هناك",
+            "visitor": "visit يزور \nvisitor زائر",
+            "knife": "سكينة",
+            "fork": "شوكة",
+            "and": "و"
+
+        },
+    },
+    "about": {
+        "translation": " حوالي أو عن",
+        "examples": ["It costs about 10 $", "They waited (for) about an hour", "He arrived (at) about ten.", "Are you talking about residence permit?", "What are you talking about?", "Yes, I am talking about residence permit"],
+        "examplesToAr": ["هي تكلف حوالي ١٠ دولار", "هم انتظروا (لـ) حوالي ساعةً", "هو وصل حوالي (بـ) العاشرة", "عم تتحدث عن تصريح الإقامة؟", "ماذا تتحدث عن؟", "نعم، أنا أتحدث عن تصريح الإقامة"],
+        "dict": {
+            "It": "هي أو هو لغير العاقل",
+            "costs": "تكلف",
+            "for": "لـ",
+            "(for)": "(لـ)",
+            "They": "هم",
+            "waited": "wait ينتظر \n ed مشان تصير ماضي \nwaited  انتظر \n",
+            "about": "عن أو حوالي",
+            "hour": "ساعة",
+            "arrived": "وصل",
+            "(at)": "(بـ)",
+            "ten": "عشرة",
+            "Are": "عم",
+            "you": "أنت أو أنتم",
+            "talking": "تتحدث",
+            "residence": "إقامة",
+            "permit?": "تصريح؟",
+            "What": "ماذا/شو",
+            "Yes,": "نعم،",
+            "I": "أنا",
+            "am": " I تجي مع ",
+        },
+    },
+    "above": {
+        "translation": "فوق",
+        "examples": ["70 or above.", "We were flying above the clouds.", "Your health is above everything.",],
+        "examplesToAr": ["سبعين أو فوق", "نحن كنا نطير فوق الغيوم", "صحتك فوق كل شيء",],
+        "dict": {
+            "or": "أو",
+            "above": "فوق",
+            "above.": "فوق",
+            "We": "نحن",
+            "were": "كنا/كان",
+            "flying": "نطير",
+            "the": "الـ",
+            "clouds.": "الغيوم.",
+            "Your": "ـك\n your health صحتك",
+            "health": "صحة",
+            "everything.": "كل شيء",
+        },
+    },
+
+
+    "action": {
+        "translation": "فعل",
+        "examples": ["Actions speak louder than words",],
+        "examplesToAr": ["الأفعال أعلى صوتا من الكلمات"],
+        "dict": {
+            "actions": "أفعال",
+            "action": "فعل",
+            "Actions": "أفعال",
+            "speak": "يتكلم",
+            "louder": "أعلى صوتا",
+            "than": "من",
+            "words": "كلمات",
+        },
+    },
+    "active": {
+        "translation": "فعل",
+        "examples": ["He is an active person"],
+        "examplesToAr": [" هو شخصٌ نشط"],
+        "dict": {
+            "active": "نشط",
+            "He": "هو",
+            "is": "",
+            "an": " تنوين ـٌ",
+            "a": " تنوين ـٌ",
+            "person": "شخصٌ",
+        }
+    },
+    "actor": {
+        "translation": "ممثل أو فاعل (حسب السياق)",
+        "examples": [],
+        "examplesToAr": [],
+        "dict": {
+            "actor": "ممثل أو فاعل (حسب السياق)",
+        }
+    },
+    "actress": {
+        "translation": "ممثلة أو فاعلة (حسب السياق)",
+        "examples": [],
+        "examplesToAr": [],
+        "dict": { "actress": "ممثلة أو فاعلة (حسب السياق)" }
+    },
+    "add": {
+        "translation": "يضيف",
+        "examples": ["Please, add my name to the list.", "Add 9 to the total."],
+        "examplesToAr": ["رجاء، أضف اسمي إلي الليستا (يعني المجموعة)", "أضف ٩ إلى المجموع"],
+        "dict": {
+            "Please,": "رجاء",
+            "add": "أضف",
+            "my": "ـي",
+            "name": "اسمي",
+            "to": "إلى",
+            "the": "الـ",
+            "list.": "الليستا/المجموعة",
+            "Add": "أضف",
+            "total.": "المجموع",
+        }
+    },
+    "address": {
+        "translation": "عنوان",
+        "examples": ["What is your address?"],
+        "examplesToAr": ["ما هو عنوانك؟"],
+        "dict": {
+            "What": "ما/ماذا",
+            "is": "هو",
+            "your": "ـك",
+            "address?": "عنوان",
+            "address": "عنوان",
+
+        }
+    },
+    "afraid": {
+        "translation": "خائف",
+        "examples": ["Don't be afraid to ask if you don't understand."],
+        "examplesToAr": ["لا تكن خائف أن تسأل إذا أنت لا تفهم",],
+        "dict": {
+            "Don't": "لا",
+            "be": "تكن/يكون",
+            "afraid": "خائف",
+            "to": "إلى/أن/لـ",
+            "ask": "يسأل",
+            "if": "إذا",
+            "you": "أنت/أنتم",
+            "don't": "لا",
+            "understand.": "يفهم",
+
+        },
+    },
+    "after": {
+        "translation": "بعد",
+        "examples": ["We will leave after lunch."],
+        "examplesToAr": ["نحن سوف نغادر بعد الغداء"],
+        "dict": {
+            "We": "نحن",
+            "will": "سوف",
+            "leave": "نغادر",
+            "after": "بعد",
+            "lunch.": "الغداء",
+        }
+
+    },
+    "all": {
+        "translation": "كل",
+        "examples": ["All people originate from Adam and Eve.",],
+        "examplesToAr": ["كل الناس ينحدرون/يتجذرون من آدم وحواء",],
+        "notes": "<span class='wordsChildOfNotes'>origin </span> <span class='speakIcon'> 🔊 </span>: اسم بمعنى أصل/جذر <br> <span class='wordsChildOfNotes'>originate </span> <span class='speakIcon'> 🔊 </span>: فعل بمعنى يتجذر/ينحدر",
+        "dict": {
+            "All": "كل",
+            "all": "كل",
+            "people": "الناس",
+            "originate": "originateينحدر\noriginأصل",
+            "from": "من",
+            "Adam": "آدم",
+            "and": "و",
+            "Eve.": "حواء",
+        },
+    },
+    "another": {
+        "translation": "آخر",
+        "examples": ["That's another thing.",],
+        "examplesToAr": ["ذلك شيء آخر"],
+        "dict": {
+            "That's": "ذلك",
+            "another": "آخر",
+            "thing.": "شيء",
+        }
+    }
+
+
+}
+

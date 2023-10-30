523c91f61caa7d1625038a7f43fd5972adc62c8e
truthpapers <tableofdawah@outlook.com>
2023-10-30 14:03:27 +0100

    fix

---
+++ components.js
@@ -0,0 +1,11 @@
+export const header = function (activeTab) {
+    return `
+        <a ${activeTab === 'alphabet' ? 'style="border-bottom: 1px solid gray"' : ''} href="alphabet.html">درس الحروف والهجاء</a>
+        <a ${activeTab === 'airport' ? 'style="border-bottom: 1px solid gray"' : ''} href="airport.html"> في المطار </a>
+        <a ${activeTab === 'health_problems' ? 'style="border-bottom: 1px solid gray"' : ''} href="health_problems.html"> الصحة </a>
+        <a ${activeTab === 'directions' ? 'style="border-bottom: 1px solid gray"' : ''} href="directions.html"> الاتجاهات </a>
+        <a ${activeTab === 'do_are' ? 'style="border-bottom: 1px solid gray"' : ''} href="do_are.html"> are/do </a>
+        <a ${activeTab === 'oxford3000' ? 'style="border-bottom: 1px solid gray"' : ''} href="oxford3000.html">Oxford 3000</a>
+        <a ${activeTab === 'conversations' ? 'style="border-bottom: 1px solid gray"' : ''} href="conversations.html">محادثات</a>
+    `;
+}

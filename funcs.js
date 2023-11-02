e812a6cbe7b9dabd398609bff3a53fda92b9a049
truthpapers <tableofdawah@outlook.com>
2023-11-02 22:40:15 +0100

    fix

--- funcs.js
+++ funcs.js
@@ -1,16 +1,29 @@
-export function wordExplainPopup(e, obj, popupBox, popupArrow) {
-    const dict = obj
+export function wordExplainPopup(e, dict) {
+    const popupArrow = document.createElement("div")
+    popupArrow.id = "popupArrow"
+    const popupBox = document.createElement("div")
+    popupBox.id = "popupBox"
+     if (document.getElementById("popupBox")) {
+        document.body.replaceChild(popupArrow, document.getElementById("popupArrow") )
+        document.body.replaceChild(popupBox, document.getElementById("popupBox") )
+    } else {
+        document.body.appendChild(popupBox)
+        document.body.appendChild(popupArrow)
+    } 
+
+
+
     function getElementDocumentPosition(element) {
         const rect = element.getBoundingClientRect();
-    
+
         // Get the current scroll position of the document
         const scrollX = window.scrollX || window.pageXOffset;
         const scrollY = window.scrollY || window.pageYOffset;
-    
+
         // Calculate the position relative to the document
         const top = rect.top + scrollY;
         const left = rect.left + scrollX;
-    
+
         return { top, left, width: rect.width, height: rect.height, right: rect.right + scrollX, bottom: rect.bottom + scrollY };
     }
 
@@ -59,8 +72,8 @@ export function wordExplainPopup(e, obj, popupBox, popupArrow) {
 
     popupBox.addEventListener("click", e => e.stopPropagation());
     document.addEventListener("click", e => {
-        popupBox.style.display = "none";
-        popupArrow.style.display = "none";
+        popupBox.remove()
+        popupArrow.remove()
     });
 }
 

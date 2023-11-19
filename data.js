afc75f0ae2876133a10593b10b7d9f27f5d781db
truthpapers <tableofdawah@outlook.com>
2023-11-19 15:15:19 +0100

    new lesson

--- data.js
+++ data.js
@@ -2142,6 +2142,47 @@ export const conversations = [
     }
 ],
 
+[
+    [
+        "Can you recommend a good book?",
+        "Yes, I recommend a book called 'Fath Al-Majid' (فتح المجيد شرح كتاب التوحيد). <br> <br> Try finding it in a mosque or buy it.",
+        "Can you send me a PDF version?",
+        "Yes, here drive.google.com/file/d/1Ax8CQ7UDjitfKsiw9Hh9Al3W4zEf1FP9"
+    ],
+    [
+        "تقدر تنصح (تزكي/توصي) بكتابٍ كويس؟",
+        "نعم، أوصي بكتاب يسمى 'فتح المجيد' (فتح المجيد شرح كتاب التوحيد). <br> <br> حاول  إيجاده في مسجدٍ ما أو أشتريه.",
+        "تقدر ترسل لي نسخة PDF؟",
+        "نعم، هنا drive.google.com/file/d/1Ax8CQ7UDjitfKsiw9Hh9Al3W4zEf1FP9."
+    ],
+    {
+        "can": "هل",
+        "you": "يمكنك",
+        "recommend": "توصي/تزكي/تنصح",
+        "a": "تنوين",
+        "good": "كويس",
+        "book": "كتاب",
+        "called": "يسمى",
+        "fath": "'فتح",
+        "al-majid": "المجيد",
+       
+        "try": "حاول",
+        "finding": "إيجاد",
+        "it": "ـه / ـها ",
+        "in": "في",
+        "mosque": "مسجد",
+        "or": "أو",
+        "buy": "يشتري",
+        "send": "يرسل",
+        "me": "لي",
+        
+        "pdf": "pdf",
+        "version": "نسخة",
+        "yes": "نعم",
+        "here": "هنا",
+    }
+],
+
     
 ]
 

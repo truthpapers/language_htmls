a414ab52ac94ded35e738d4e44a20db4673045af
truthpapers <tableofdawah@outlook.com>
2023-11-26 18:31:00 +0100

    fix

--- data.js
+++ data.js
@@ -2441,13 +2441,13 @@ export const conversations = [
         "What do you advise me to study?",
         "There are many courses on Youtube.",
         "What do you recommend?",
-        "Learn how to build websites and apps <br><br> www.youtube.com/watch?v=o3lXuw3jRAo&list=PLPrGWEF_l-5NWKkwmivJnnbA6u6xxZNJ_ </a>"
+        "Learn how to build websites and apps <br><br> https://youtube.com/playlist?list=PLPrGWEF_l-5NWKkwmivJnnbA6u6xxZNJ_&si=qKcuuk00g01Gryl7 </a>"
     ],
     [
         "ماذا تنصحني أن أدرس؟ <br> do للدلالة على أن السؤال بشكل عام. ",
         "يوجد كثير كورسات على يوتيوب. <br>يوجد = there are ",
         "ماذا توصي؟",
-        "تعلم كيف تبني مواقع وبرامج <br> www.youtube.com/watch?v=o3lXuw3jRAo&list=PLPrGWEF_l-5NWKkwmivJnnbA6u6xxZNJ_ ",
+        "تعلم كيف تبني مواقع وبرامج <br> https://youtube.com/playlist?list=PLPrGWEF_l-5NWKkwmivJnnbA6u6xxZNJ_&si=qKcuuk00g01Gryl7 ",
     ],
     {
         "what": "ماذا",
